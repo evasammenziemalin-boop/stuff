c3d32e18c2bd220e7115e3d472c6561bbeb96b6e
evasammenziemalin-boop <evasammenziemalin@gmail.com>
2025-11-09 11:32:24 +0000

    Update code (1).js

--- code (1).js
+++ code (1).js
@@ -55,14 +55,14 @@ class ScratchCustom {
 	        			defaultValue: "1"
 	        		}
 	        	},
-	        },	
+	        }	
 	    	]
 	    } // use arrow keys to scroll down
 	} 
 
 	power({a,b}, util) {
 		return(a ** b)
-	},
+	}
 	pi({c}, util) {
 		return(3.1415926535 * 2)
 	}

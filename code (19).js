b1d1bed5546d5d63bfc106a6861db252e8913fb2
evasammenziemalin-boop <evasammenziemalin@gmail.com>
2025-11-10 21:23:38 +0000

    color test

---
+++ code (19).js
@@ -0,0 +1,96 @@
+
+    // =================== Scratch extension =================== 
+
+// auto arguments is a little over complicated to deduce argument count
+
+const letter = i => String.fromCharCode(97+i)
+
+const auto_block = (blockType, opcode, text, args) => ({
+	blockType,
+	opcode,
+	text,
+	arguments: Object.fromEntries(
+		new Array(text.split('[').length-1).fill().map((_,i)=> [
+			letter(i), {
+				type: (args && args[i]) || "number", 
+				defaultValue: " "
+			}
+		])
+	)
+})
+
+class ScratchCustom {
+
+	constructor(runtime) {
+		this.runtime = runtime
+	}
+
+	getInfo() {
+    	    return {
+	    	id: "math", // any other id breaks it, idk why
+	    	name: "Color",
+	    	color1: '#ff7777', 
+	    	color2: '#ee6666', 
+	    	color3: '#dd5555', 
+	    	blocks: [
+	        {
+	        	blockType: 'command',
+	        	opcode: 'Out',
+	        	text: 'output [a] to [b]',
+	        	arguments: {
+	        		a: {
+	        			type: "number",
+	        			defaultValue: " "
+	        		},
+	        		b: {
+	        			type: "string",
+	        			defaultValue:" "
+	        		}
+	        	}
+	        },
+	      	
+	    	]
+	    } // use arrow keys to scroll down
+	} 
+
+	Out({a,b}, util) {
+		let variable = util.target.lookupOrCreateList(undefined, b);
+	    if(variable)
+       		variable.value = a.split(' ');
+	}
+    }
+
+// ============== globalize vm and load extension ===============
+
+function findReactComponent(element) {
+    let fiber = element[Object.keys(element).find(key => key.startsWith("__reactInternalInstance$"))];
+    if (fiber == null) return null;
+
+    const go = fiber => {
+        let parent = fiber.return;
+        while (typeof parent.type == "string") {
+            parent = parent.return;
+        }
+        return parent;
+    };
+    fiber = go(fiber);
+    while(fiber.stateNode == null) {
+        fiber = go(fiber);
+    }
+    return fiber.stateNode;
+}
+
+window.vm = (node => {
+	  node = document.querySelector('div[class*=stage-header_stage-header-wrapper]');
+	  node = node[Object.keys(node).find(key => (key = String(key), key.startsWith('__reactInternal') || key.startsWith('__reactFiber')))].return.return.return;
+	  node = node.stateNode?.props?.vm || node.return?.updateQueue?.stores?.[0]?.value?.vm;
+	  if (!node) throw new Error('Could not find VM :(');
+	  return node;
+})();
+
+(function() {
+    var extensionInstance = new ScratchCustom(window.vm.extensionManager.runtime)
+    var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
+    window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
+})()
+    
